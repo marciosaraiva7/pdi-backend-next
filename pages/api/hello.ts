63d2f5a8ad6730b40a6d084ad12c9ef75faf9b8f
Márcio Saraiva <73492851+marciosaraiva7@users.noreply.github.com>
2022-12-17 15:14:21 -0300

    cors testing

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,15 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
+import cors from "cors";
 
 type Data = {
-  name: string
-}
+  name: string;
+};
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  cors({ origin: "http://127.0.0.1:5173/?" });
+  res.status(200).json({ name: "John Doe" });
 }

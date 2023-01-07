17517d983422e4be7c7988390f8a32980d1aed98
marciosaraiva7 <marciohm7@gmail.com>
2023-01-07 18:17:10 -0300

    open cors

--- pages/api/auth/register.ts
+++ pages/api/auth/register.ts
@@ -2,15 +2,41 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import bcrypt from "bcrypt";
 import connect from "../../../utils/connectDB";
+import Cors from "cors";
 
 type Data = {
   msg: string;
 };
 
+
+const cors = Cors({
+  methods: ["POST", "GET", "HEAD"],
+});
+
+function runMiddleware(
+  req: NextApiRequest,
+  res: NextApiResponse,
+  fn: Function
+) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result: any) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+
+      return resolve(result);
+    });
+  });
+}
+
+
 export default async function register(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
+
+  await runMiddleware(req, res, cors);
+  
   if (req.method === "POST") {
     // const { name, email, password, confirmpassword } = body;
     const body = req.body;
